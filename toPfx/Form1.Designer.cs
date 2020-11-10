830282cb41242e18686fdb9ddefeb7dc0183b2d3
Jose Kabo <kabo.0699@gmail.com>
2020-11-10 03:01:44 -0600

    Agregar archivos de proyecto.

---
+++ toPfx/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace toPfx
+{
+    partial class body
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(body));
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.header = new System.Windows.Forms.Panel();
+            this.container = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_notation = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_submit = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lbl_topfx = new System.Windows.Forms.Label();
+            this.lbl_toptfx = new System.Windows.Forms.Label();
+            this.header.SuspendLayout();
+            this.container.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 2;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // header
+            // 
+            this.header.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(107)))), ((int)(((byte)(107)))));
+            this.header.Controls.Add(this.label3);
+            this.header.Controls.Add(this.label2);
+            this.header.Controls.Add(this.label1);
+            this.header.Dock = System.Windows.Forms.DockStyle.Top;
+            this.header.Location = new System.Drawing.Point(0, 0);
+            this.header.Name = "header";
+            this.header.Size = new System.Drawing.Size(678, 188);
+            this.header.TabIndex = 0;
+            // 
+            // container
+            // 
+            this.container.Controls.Add(this.panel1);
+            this.container.Controls.Add(this.btn_submit);
+            this.container.Controls.Add(this.label4);
+            this.container.Controls.Add(this.txt_notation);
+            this.container.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.container.Location = new System.Drawing.Point(0, 188);
+            this.container.Name = "container";
+            this.container.Size = new System.Drawing.Size(678, 329);
+            this.container.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Poppins", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(47, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 51);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "toNotation()";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Poppins Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(52, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "parser.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Cursor = System.Windows.Forms.Cursors.No;
+            this.label3.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(634, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 28);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "x";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txt_notation
+            // 
+            this.txt_notation.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txt_notation.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_notation.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(107)))), ((int)(((byte)(107)))));
+            this.txt_notation.HintForeColor = System.Drawing.Color.Empty;
+            this.txt_notation.HintText = "Notación infija";
+            this.txt_notation.isPassword = false;
+            this.txt_notation.LineFocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(107)))), ((int)(((byte)(107)))));
+            this.txt_notation.LineIdleColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(107)))), ((int)(((byte)(107)))));
+            this.txt_notation.LineMouseHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(107)))), ((int)(((byte)(107)))));
+            this.txt_notation.LineThickness = 4;
+            this.txt_notation.Location = new System.Drawing.Point(101, 61);
+            this.txt_notation.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.txt_notation.Name = "txt_notation";
+            this.txt_notation.Size = new System.Drawing.Size(474, 43);
+            this.txt_notation.TabIndex = 0;
+            this.txt_notation.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Poppins Light", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Gray;
+            this.label4.Location = new System.Drawing.Point(97, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(160, 22);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ingrese una cadena infija";
+            // 
+            // btn_submit
+            // 
+            this.btn_submit.ActiveBorderThickness = 1;
+            this.btn_submit.ActiveCornerRadius = 3;
+            this.btn_submit.ActiveFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(107)))), ((int)(((byte)(107)))));
+            this.btn_submit.ActiveForecolor = System.Drawing.Color.White;
+            this.btn_submit.ActiveLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(107)))), ((int)(((byte)(107)))));
+            this.btn_submit.BackColor = System.Drawing.Color.White;
+            this.btn_submit.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_submit.BackgroundImage")));
+            this.btn_submit.ButtonText = "Generar Notaciones";
+            this.btn_submit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_submit.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_submit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(107)))), ((int)(((byte)(107)))));
+            this.btn_submit.IdleBorderThickness = 1;
+            this.btn_submit.IdleCornerRadius = 3;
+            this.btn_submit.IdleFillColor = System.Drawing.Color.White;
+            this.btn_submit.IdleForecolor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(107)))), ((int)(((byte)(107)))));
+            this.btn_submit.IdleLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(107)))), ((int)(((byte)(107)))));
+            this.btn_submit.Location = new System.Drawing.Point(394, 114);
+            this.btn_submit.Margin = new System.Windows.Forms.Padding(5);
+            this.btn_submit.Name = "btn_submit";
+            this.btn_submit.Size = new System.Drawing.Size(181, 41);
+            this.btn_submit.TabIndex = 4;
+            this.btn_submit.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btn_submit.Click += new System.EventHandler(this.btn_submit_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 179);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(678, 150);
+            this.panel1.TabIndex = 5;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 4.624278F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 95.37572F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 311F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.label7, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lbl_topfx, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lbl_toptfx, 2, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 32.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 67.33334F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(678, 150);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label5.Location = new System.Drawing.Point(349, 21);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 28);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "toPostfix()";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label7.Location = new System.Drawing.Point(19, 21);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(91, 28);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "toPrefix()";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lbl_topfx
+            // 
+            this.lbl_topfx.AutoSize = true;
+            this.lbl_topfx.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbl_topfx.Font = new System.Drawing.Font("Poppins", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_topfx.ForeColor = System.Drawing.Color.Black;
+            this.lbl_topfx.Location = new System.Drawing.Point(19, 49);
+            this.lbl_topfx.Name = "lbl_topfx";
+            this.lbl_topfx.Size = new System.Drawing.Size(324, 101);
+            this.lbl_topfx.TabIndex = 6;
+            this.lbl_topfx.Text = "cd-ab+*";
+            this.lbl_topfx.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lbl_toptfx
+            // 
+            this.lbl_toptfx.AutoSize = true;
+            this.lbl_toptfx.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbl_toptfx.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_toptfx.ForeColor = System.Drawing.Color.Black;
+            this.lbl_toptfx.Location = new System.Drawing.Point(349, 49);
+            this.lbl_toptfx.Name = "lbl_toptfx";
+            this.lbl_toptfx.Size = new System.Drawing.Size(305, 101);
+            this.lbl_toptfx.TabIndex = 7;
+            this.lbl_toptfx.Text = "ab+cd-*";
+            this.lbl_toptfx.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // body
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(678, 517);
+            this.Controls.Add(this.container);
+            this.Controls.Add(this.header);
+            this.Font = new System.Drawing.Font("Poppins", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "body";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "toPFX";
+            this.header.ResumeLayout(false);
+            this.header.PerformLayout();
+            this.container.ResumeLayout(false);
+            this.container.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private System.Windows.Forms.Panel container;
+        private System.Windows.Forms.Panel header;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private Bunifu.Framework.UI.BunifuThinButton2 btn_submit;
+        private System.Windows.Forms.Label label4;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox txt_notation;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbl_topfx;
+        private System.Windows.Forms.Label lbl_toptfx;
+    }
+}
+
---
+++ toPfx/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace toPfx
+{
+    public partial class body : Form
+    {
+        public body()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_submit_Click(object sender, EventArgs e)
+        {
+            proccessingEntry(txt_notation.Text);
+        }
+
+        private Stack<String> stack_pfx_operands = new Stack<string>();
+        private Stack<String> stack_pfx_operators = new Stack<string>();
+
+        private Stack<String> stack_ptfx = new Stack<string>();
+
+        private string[] operatorsAccepted  = {"+","-", "/","*"};
+
+        void proccessingEntry(string cadena)
+        {
+            this.lbl_topfx.Text = this.toPrefix(cadena);
+            this.lbl_toptfx.Text = this.toPostfix(this.lbl_topfx.Text);
+        }
+
+        string toPrefix(string cadena)
+        {
+            for (int i = 0; i < cadena.Length; i++)
+            {
+                if (cadena[i] == '(')
+                {
+                    stack_pfx_operators.Push(cadena[i].ToString());
+                }
+                else if (cadena[i] == ')')
+                {
+                    while (stack_pfx_operators.Count != 0 && stack_pfx_operators.Peek() != "(")
+                    {
+                        String op1 = stack_pfx_operands.Peek();
+                        stack_pfx_operands.Pop();
+
+                        String op2 = stack_pfx_operands.Peek();
+                        stack_pfx_operands.Pop();
+
+                        string op = stack_pfx_operators.Peek();
+                        stack_pfx_operators.Pop();
+
+                        String tmp = op + op2 + op1;
+                        stack_pfx_operands.Push(tmp);
+                    }
+                    stack_pfx_operators.Pop();
+                }
+                else if (!this.operatorsAccepted.Contains(cadena[i].ToString()))
+                {
+                    stack_pfx_operands.Push(cadena[i] + "");
+                }
+                else
+                {
+                    while (stack_pfx_operators.Count != 0 && getOperatorPriority(cadena[i]) <= getOperatorPriority(stack_pfx_operators.Peek()[0]))
+                    {
+
+                        String op1 = stack_pfx_operands.Pop();
+                        String op2 = stack_pfx_operands.Pop();
+
+                        string op = stack_pfx_operators.Pop();
+
+                        String tmp = op + op2 + op1;
+                        stack_pfx_operands.Push(tmp);
+                    }
+                    stack_pfx_operators.Push(cadena[i].ToString());
+                }
+            }
+
+            while (stack_pfx_operators.Count != 0)
+            {
+                String op1 = stack_pfx_operands.Peek();
+                stack_pfx_operands.Pop();
+
+                String op2 = stack_pfx_operands.Peek();
+                stack_pfx_operands.Pop();
+
+                string op = stack_pfx_operators.Peek();
+                stack_pfx_operators.Pop();
+
+                String tmp = op + op2 + op1;
+                stack_pfx_operands.Push(tmp);
+            }
+            return this.rotateEntry(stack_pfx_operands.Peek());
+        }
+
+        string rotateEntry(string entry)
+        {
+            char[] cadenaComoCaracteres = entry.ToCharArray();
+            Array.Reverse(cadenaComoCaracteres);
+            return new string(cadenaComoCaracteres);
+        }
+
+        string toPostfix(string cadena)
+        {
+            return "Pendiente";
+        }
+
+        int getOperatorPriority(char C)
+        {
+            if (C == '-' || C == '+')
+                return 1;
+            else if (C == '*' || C == '/')
+                return 2;
+            else
+                return 0;
+        }
+    }
+}
